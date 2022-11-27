e697fbdbb9d9cc5da5ac247685c3d05f5b5ae0a5
Lenovo <18720862783@qq.com>
2022-11-27 21:14:56 +0800

    game

--- app/src/main/java/com/jnu/booklistmainactivity/BookListMainActivity.java
+++ app/src/main/java/com/jnu/booklistmainactivity/BookListMainActivity.java
@@ -1,49 +1,19 @@
 package com.jnu.booklistmainactivity;
-
-import static com.jnu.booklistmainactivity.BookListMainActivity.MENU_ID_ADD;
-import static com.jnu.booklistmainactivity.BookListMainActivity.MENU_ID_DELETE;
-import static com.jnu.booklistmainactivity.BookListMainActivity.MENU_ID_UPDATE;
-
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Lifecycle;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.annotation.SuppressLint;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
-import com.jnu.booklistmainactivity.data.Book;
-import com.jnu.booklistmainactivity.data.DataSaver;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class BookListMainActivity extends AppCompatActivity {
-    public static final int MENU_ID_ADD=1;
-    public static final int MENU_ID_UPDATE=2;
-    public static final int MENU_ID_DELETE=3;
-    private ArrayList<Book> books;//书本列表
-
     //Fragement适配器,PageViewFragmentAdapater类
     public class PageViewFragmentAdapater extends FragmentStateAdapter{
         public PageViewFragmentAdapater(@NonNull FragmentManager fragmentManager,@NonNull Lifecycle lifecycle){
@@ -61,13 +31,15 @@ public class BookListMainActivity extends AppCompatActivity {
                     return WebViewFragment.newInstance();
                 case 2:
                     return MapViewFragment.newInstance();
+                case 3:
+                    return GameFragment.newInstance();
             }
             return BooklistFragment.newInstance();
         }
 
         @Override
         public int getItemCount() {
-            return 3;
+            return 4;
         }
     }
 
@@ -93,6 +65,9 @@ public class BookListMainActivity extends AppCompatActivity {
                         break;
                     case 2:
                         tab.setText("地图");
+                        break;
+                    case 3:
+                        tab.setText("游戏");
                         break;
                 }
             }
